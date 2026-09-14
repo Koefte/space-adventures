838c4a7392d87a9f3a13c81199ec567e352f8c7c
anb3li <anbeli260907@gmail.com>
2026-09-14 14:51:23 +0200

    heligie scheise

--- scenes/spaceship.gd
+++ scenes/spaceship.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 @export var move_speed: float = 3.0
 @export var steer_speed = 0.5
 @export var steer_rotation_factor = 50
-@export var deceleration_factor = 0.2
+@export var deceleration_factor = 0.8
 
 var steer_value = 0
 var movement = Vector3.ZERO
@@ -12,15 +12,15 @@ var movement = Vector3.ZERO
 @onready var ship_node = $CSGBox3D
 
 func _physics_process(delta: float) -> void:
-	
-	if Input.is_action_pressed("move_forward"):
+	return
+	if false:
 		movement = Vector3.FORWARD * move_speed
 	else :
-		movement.z = lerpf(movement.z, 0, deceleration_factor * delta * delta)
+		movement.z = lerpf(movement.z, 0, deceleration_factor * delta)
 	
-	if Input.is_action_pressed("move_left"):	
+	if false:	
 		steer_value = lerpf(steer_value, 1,steer_speed*delta)
-	elif Input.is_action_pressed("move_right"):
+	elif false:
 		steer_value = lerpf(steer_value, -1,steer_speed*delta)
 		
 	rotation_degrees.z = steer_value * 45
--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,4 +1,4 @@
-extends CharacterBody3D
+extends Node3D
 
 @export var move_speed: float = 5.0
 @export var jump_velocity: float = 4.5
@@ -31,35 +31,3 @@ func _unhandled_input(event: InputEvent) -> void:
 		head.rotate_y(-event.relative.x * mouse_sensitivity)
 		camera.rotate_x(-event.relative.y * mouse_sensitivity)
 		camera.rotation.x = clampf(camera.rotation.x, -deg_to_rad(85), deg_to_rad(85))
-
-
-
-func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	
-			
-
-	
-	
-	
-	
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-
-
-	
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction = (head.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	
-	
-	if direction:
-		velocity.x = lerpf(velocity.x, direction.x * move_speed, acceleration * delta)
-		velocity.z = lerpf(velocity.z, direction.z * move_speed, acceleration * delta)
-	else:
-		velocity.x = lerpf(velocity.x, 0.0, acceleration * delta)
-		velocity.z = lerpf(velocity.z, 0.0, acceleration * delta)
-
-	move_and_slide()
