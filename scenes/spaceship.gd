907b0b11096f619d5ebd9fe60178d26f9eadf23b
anb3li <anbeli260907@gmail.com>
2026-09-14 13:22:58 +0200

    base movement

--- scenes/spaceship.gd
+++ scenes/spaceship.gd
@@ -1,23 +1,33 @@
 extends CharacterBody3D
 
 @export var acceleration: float = 10.0
-@export var move_speed: float = 10.0
-@export var rotation_speed = 0.5
+@export var move_speed: float = 3.0
+@export var steer_speed = 0.5
+@export var steer_rotation_factor = 50
+@export var deceleration_factor = 0.2
+
+var steer_value = 0
+var movement = Vector3.ZERO
 
 @onready var ship_node = $CSGBox3D
 
 func _physics_process(delta: float) -> void:
+	
 	if Input.is_action_pressed("move_forward"):
-		velocity.z = lerpf(velocity.z, -move_speed, acceleration * delta)
-	else:
-		velocity.z =  lerpf(velocity.z,0,acceleration*delta)
+		movement = Vector3.FORWARD * move_speed
+	else :
+		movement.z = lerpf(movement.z, 0, deceleration_factor * delta * delta)
+	
 	if Input.is_action_pressed("move_left"):	
-		rotation.z = lerpf(rotation.z,deg_to_rad(45.0),rotation_speed*delta)
+		steer_value = lerpf(steer_value, 1,steer_speed*delta)
 	elif Input.is_action_pressed("move_right"):
-		rotation.z = lerpf(rotation.z,deg_to_rad(-45.0),rotation_speed*delta)
-	var ship_tilt = rad_to_deg(rotation.z)
-	print(ship_tilt,velocity.z)
-	if abs(ship_tilt) > 5.0 and abs(velocity.z) > 0.1:
-		print("Got here",ship_tilt,velocity.z)
-		rotation.y += deg_to_rad((rad_to_deg(rotation.z) / 45.0 )* 60.0) * delta
+		steer_value = lerpf(steer_value, -1,steer_speed*delta)
+		
+	rotation_degrees.z = steer_value * 45
+	
+	if movement.length() > 0.1:
+		rotation_degrees.y += steer_value * steer_rotation_factor * delta
+		
+	velocity = movement.z * global_transform.basis.z
+	
 	move_and_slide()
